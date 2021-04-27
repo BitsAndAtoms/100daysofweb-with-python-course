746b5c167adfb62cca0f1b133d437185c043dd51
Sid Gupta <siddhartha.che@gmail.com>
2021-04-27 11:58:37 -0500

    my first commit

--- days/001-004-flask-intro/code/program/routes.py
+++ days/001-004-flask-intro/code/program/routes.py
@@ -1,11 +1,14 @@
 from flask import render_template
 from program import app
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html', title='Template Demo', time=timenow)
 
-@app.route('/100Days')
+@app.route("/")
+@app.route("/index")
+def index():
+    # return "pythonista"
+    return render_template("index.html", title="Template Demo", time=None)
+
+
+@app.route("/100Days")
 def p100days():
-    return render_template('100Days.html')
+    return render_template("100Days.html")
